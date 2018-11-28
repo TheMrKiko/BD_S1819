f67ba19442c381fedabbcda0b66ff0d4a764618e
Francisco Sousa <f.sou@hotmail.com>
2018-11-28 15:09:38 +0000

    Corrigido populate e schema
    populate: timestamps e time
    schema: restrições a mais e a menos

--- 3ª entrega/schema.sql
+++ 3ª entrega/schema.sql
@@ -24,19 +24,18 @@ create table camara
 create table video
     (data_hora_inicio timestamp not null,
      data_hora_fim    timestamp not null,
-     num_camara       char(5)   not null unique,
+     num_camara       char(5)   not null,
      constraint pk_video primary key(data_hora_inicio, num_camara),
      constraint fk_num_camara foreign key(num_camara) references camara(num_camara),
      constraint ck_data_hora check (data_hora_inicio < data_hora_fim));
 
 create table segmento_video
     (num_segmento     char(5)    not null,
-     duracao          varchar(5) not null,
+     duracao          int not null,
      data_hora_inicio timestamp  not null,
-     num_camara       char(5)    not null unique,
+     num_camara       char(5)    not null,
      constraint pk_segmento_video primary key(num_segmento, data_hora_inicio, num_camara),
-     constraint fk_segmento_video foreign key(data_hora_inicio, num_camara) references video(data_hora_inicio, num_camara)
-     );
+     constraint fk_segmento_video foreign key(data_hora_inicio, num_camara) references video(data_hora_inicio, num_camara));
 
 create table localidade
     (morada_local varchar(255) not null,
@@ -68,32 +67,29 @@ create table entidade_meio
      constraint pk_nome_entidade primary key(nome_entidade));
 
 create table meio
-    (num_meio      char(5)     not null unique,
-     nome_meio     varchar(20) not null,
-     nome_entidade varchar(20) not null unique,
+    (num_meio      char(5)     not null,
+     nome_meio     varchar(255) not null,
+     nome_entidade varchar(20) not null,
      constraint pk_meio primary key(num_meio, nome_entidade),
      constraint fk_nome_entidade foreign key(nome_entidade) references entidade_meio(nome_entidade));
 
 create table meio_combate
-    (num_meio      char(5)     not null unique,
-     nome_entidade varchar(20) not null,
+    (num_meio      char(5)     not null,
+     nome_entidade varchar(255) not null,
      constraint pk_meio_combate primary key(num_meio, nome_entidade),
-     constraint fk_num_meio      foreign key(num_meio)      references meio(num_meio),
-     constraint fk_nome_entidade foreign key(nome_entidade) references meio(nome_entidade));
+     constraint fk_meio      foreign key(num_meio, nome_entidade)      references meio(num_meio, nome_entidade));
 
 create table meio_apoio
     (num_meio       char(5)    not null,
-     nome_entidade  varchar(20) not null,
+     nome_entidade  varchar(255) not null,
      constraint pk_apoio primary key(nome_entidade, num_meio),
-     constraint fk_apoio_num  foreign key(num_meio)      references meio(num_meio),
-     constraint fk_apoio_nome foreign key(nome_entidade) references meio(nome_entidade));
+     constraint fk_meio      foreign key(num_meio, nome_entidade)      references meio(num_meio, nome_entidade));
 
 create table meio_socorro
     (num_meio       char(5)    not null,
-     nome_entidade  varchar(20) not null,
+     nome_entidade  varchar(255) not null,
      constraint pk_socorro primary key(nome_entidade, num_meio),
-     constraint fk_socorro_num  foreign key(num_meio)      references meio(num_meio),
-     constraint fk_socorro_nome foreign key(nome_entidade) references meio(nome_entidade));
+     constraint fk_meio      foreign key(num_meio, nome_entidade)      references meio(num_meio, nome_entidade));
 
 create table transporta
     (num_meio             char(5)    not null,
@@ -118,8 +114,7 @@ create table acciona
      nome_entidade        varchar(20) not null,
      num_processo_socorro char(5),
      constraint pk_acciona primary key(num_processo_socorro, nome_entidade, num_meio),
-     constraint fk_acciona_num_meio foreign key(num_meio)             references meio(num_meio),
-     constraint fk_acciona_nome     foreign key(nome_entidade)        references meio(nome_entidade),
+     constraint fk_acciona_num_meio foreign key(num_meio, nome_entidade)             references meio(num_meio, nome_entidade),
      constraint fk_acciona_num_proc foreign key(num_processo_socorro) references processo_socorro(num_processo_socorro));
 
 create table coordenador
@@ -137,7 +132,8 @@ create table audita
      texto                varchar(100),
      constraint pk_audita primary key(id_coordenador, num_meio, nome_entidade, num_processo_socorro),
      constraint fk_audita_num_meio foreign key(num_meio, nome_entidade, num_processo_socorro) references acciona(num_meio, nome_entidade, num_processo_socorro),
-     constraint fk_audita_id_coord foreign key(id_coordenador) references coordenador(id_coordenador));
+     constraint fk_audita_id_coord foreign key(id_coordenador) references coordenador(id_coordenador),
+     constraint ck_data_autoria check (data_auditoria <= now()));
 
 create table solicita
     (id_coordenador         char(5)  not null,
